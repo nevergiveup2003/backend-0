cc73acd6d560421774014b2a7b07bc06315b79bc
hoang phuc <hoangphuc03082003@gmail.com>
2025-06-01 23:15:28 +0700

    Setup Docker & Database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,15 @@
+const { json } = require("express");
+const connection = require("../config/database");
 const getHomepage = (req, res) => {
-  // Render the homepage view
-  res.send("Welcome to the Home Page!");
+  let users = [];
+  connection.query("SELECT * FROM Users;", function (err, results, fields) {
+    users = results;
+    console.log("results", results);
+
+    // console.log("check users",users);
+    // Render the homepage view
+    res.send(JSON.stringify(users));
+  });
 };
 const getSamplePage = (req, res) => {
   res.render("sample.ejs");
--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const viewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const express = require("express");
-
 const app = express();
+const connection = require("./config/database")
 const port = process.env.PORT || 3000;
 const hostname = process.env.HOSTNAME || "localhost";
 console.log("process.end", process.env);
@@ -12,6 +12,13 @@ console.log("process.end", process.env);
 viewEngine(app);
 app.use("/", webRoutes);
 
+connection.query(
+  "SELECT * FROM Users;",
+  function(err,results,fields){
+    console.log("results", results);
+   
+  }
+)
 app.listen(port, hostname, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
